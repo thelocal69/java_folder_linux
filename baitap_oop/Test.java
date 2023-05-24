f79e578edac4fcc55375adaef331210ff5f6cab6
thelocal69 <trankhanh740@gmail.com>
2023-05-24 10:34:05 +0700

    create folder java oop

---
+++ baitap_oop/Test.java
@@ -0,0 +1,49 @@
+package baitap_oop;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		//bai1
+//		ExpressionTwo expressionTwo = new ExpressionTwo(1, 3, -1);
+//		ExpressionTwo expressionTwo2 = new ExpressionTwo(1, -2, 1);
+//		ExpressionTwo expressionTwo3 = new ExpressionTwo(6, 1, 5);
+//		System.out.println(expressionTwo.delta());
+//		expressionTwo.equaltionTest();
+//		
+//		System.out.println(expressionTwo2.delta());
+//		expressionTwo2.equaltionTest();
+//		
+//		System.out.println(expressionTwo3.delta());
+//		expressionTwo3.equaltionTest();
+		
+		//bai2
+//		Student student1 = new Student(1, "thien", 1, 1);
+//		student1.average();
+//		System.out.println(student1.toString());
+//		
+//		Student student2 = new Student(2, "hoa", 2, 2);
+//		student2.average();
+//		System.out.println("My best friend "+student2.toString());
+//		
+//		Student student3 = new Student();
+//		student3.setId(1);
+//		student3.setName("lan");
+//		student3.setScoreLT(3);
+//		student3.setScoreTH(3);
+//		student3.average();
+//		System.out.println(student3.toString());
+		
+		//bai3
+		Account account1 = new Account(1234567890, "TRAN PHI LAM", 1000000);
+		System.out.println(account1.toString());
+		System.out.println("add money: ");
+		account1.rechargeMoneyA();
+		System.out.println("withdrawal: ");
+		account1.withdrawal();
+		System.out.println("send money: ");
+		account1.sendMoney();
+	}
+
+}
